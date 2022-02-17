2fab5bda3567bc2216a3150e3241b2fb0548dfdd
Gangadevi Ch <chilamakooru.gangaiiit@gmail.com>
2022-02-17 14:31:18 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace problems
+{
+    static class flipCoin
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Day_5_Assignments");
+        }
+    }
+}
