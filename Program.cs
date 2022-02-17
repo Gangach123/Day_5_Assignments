5b1b02cb650f26829fbfe464ee97cbb41079e808
unknown <chilamakooru.gangaiiit@gmail.com>
2022-02-17 22:46:44 +0530

    Quotient_Remainder

--- Program.cs
+++ Program.cs
@@ -7,34 +7,16 @@ namespace swapcase
     {
         static void Main()
         {
-            
-
             Console.WriteLine("Enter the first number:");
             int firstNumber = Convert.ToInt32(Console.ReadLine());
             
-
-            Console.WriteLine("Enter the second number:");
+            Console.WriteLine("Enter the first number:");
             int secondNumber = Convert.ToInt32(Console.ReadLine());
-            
-            Console.WriteLine("Enter the third number:");
-            int thirdNumber = Convert.ToInt32(Console.ReadLine());
-            
+           
+            int Quotient = firstNumber / secondNumber;
+            int Reminder = firstNumber % secondNumber;
 
-
-            if (firstNumber > secondNumber && firstNumber > thirdNumber)
-            {
-                Console.WriteLine($"The largest number is: {firstNumber}");
-            }
-            else if (secondNumber > firstNumber && secondNumber > thirdNumber)
-            {
-                Console.WriteLine($"The largest number is: {secondNumber}");
-            }
-            else
-            {
-                Console.WriteLine($"The largest number is:  {thirdNumber}");
-            }
-
-
+            Console.WriteLine($"The quotient value : {Quotient} and the reminder value:{Reminder}");
         }
     }
 }
