ca3de2fce0feeebe67f14d0b7249836309925aa2
unknown <chilamakooru.gangaiiit@gmail.com>
2022-02-17 19:07:24 +0530

    LeapYear

---
+++ leapYear.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace leap_Year
+{
+    static class LeapYear
+    {
+        static void Main()
+        {
+            
+            Console.WriteLine("Enter a year:");
+            int year = Convert.ToInt32(Console.ReadLine());
+
+            if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
+            {
+                Console.WriteLine("Entered year is a Leap Year");
+            }
+            else
+            {
+                Console.WriteLine("Entered year is not a Leap Year");
+            }
+        }
+    }
+}
