cae3d753fb1c8944f80dfcfb277ddd1ed931ccd4
unknown <chilamakooru.gangaiiit@gmail.com>
2022-02-17 23:05:45 +0530

    LargestNumber

--- Program.cs
+++ Program.cs
@@ -1,22 +1,35 @@
 ﻿using System;
 
 
-namespace swapcase
+namespace largestNumber
 {
     class Program
     {
         static void Main()
         {
+           
             Console.WriteLine("Enter the first number:");
             int firstNumber = Convert.ToInt32(Console.ReadLine());
-            
+           
             Console.WriteLine("Enter the first number:");
             int secondNumber = Convert.ToInt32(Console.ReadLine());
            
-            int Quotient = firstNumber / secondNumber;
-            int Reminder = firstNumber % secondNumber;
+            Console.WriteLine("Enter the first number:");
+            int thirdNumber = Convert.ToInt32(Console.ReadLine());
+            
+            if (firstNumber > secondNumber && firstNumber > thirdNumber)
+            {
+                Console.WriteLine($"The largest number is: {firstNumber}");
+            }
+            else if (secondNumber > firstNumber && secondNumber > thirdNumber)
+            {
+                Console.WriteLine($"The largest number is: {secondNumber}");
+            }
+            else
+            {
+                Console.WriteLine($"The largest number is:  {thirdNumber}");
+            }
 
-            Console.WriteLine($"The quotient value : {Quotient} and the reminder value:{Reminder}");
         }
     }
 }
